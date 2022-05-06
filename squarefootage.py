0a8f1a16d1bb8c66a8e08ad5a3e368ba2e368076
EC2 Default User <ec2-user@ip-172-31-85-192.ec2.internal>
2022-05-06 02:09:08 +0000

    added labs and week 13 exercises

---
+++ squarefootage.py
@@ -0,0 +1,9 @@
+#enter the variables for the width and length of the room in feet
+length=float(input("What is the length of the room in feet? "))
+width=float(input("What is the width of the room in feet? "))
+
+#formula for area
+area=length*width
+
+#message
+print("The area of the room is",area, "sq. ft")
---
+++ welcomemessage.py
@@ -0,0 +1,9 @@
+# Allow user to input their First Name and assign that input to a variable
+firstname = input("What is your first name? ")
+
+# Allow user to input their Last Name and assign that input to a variable
+lastname = input("What is your last name? ")
+
+# Print a welcome message that includes the user's First Name and Last Name using the variables.
+print('Welcome,', firstname, lastname,'!')
+
