7845e7a14467ce386a216edce7082070b9c4789d
EC2 Default User <ec2-user@ip-172-31-85-154.ec2.internal>
2022-05-09 23:10:47 +0000

    added conditionals exercises

---
+++ conditionals.py
@@ -0,0 +1,27 @@
+# >>>if 'a' < 'b':
+#...    print("CONDITION was true")
+#...
+#CONDITION was true
+#>>> if 'b' < 'a':
+#...    print("CONDITION was true")
+#...
+#>>>if false:
+#...    print("Was true")
+#... else:
+#...    print("Was false")
+#...
+#Was false
+#>>> if True:
+#...    print("Was true")
+#... else:
+#...    print("Was false")
+
+#>>> if 'b' < 'a':
+#...    print("This is true")
+#... elif 'c' < 'd':
+#...    print("Second condition is true")
+#... else:
+#...    print("no condition was true")
+#...
+#Second condition is true
+
