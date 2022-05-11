bb9600f2cbbb7610f10c72cf1b0b1b52b96b691b
EC2 Default User <ec2-user@ip-172-31-85-154.ec2.internal>
2022-05-11 00:05:16 +0000

    added tuesday call

---
+++ loops.py
@@ -0,0 +1,25 @@
+#while CONDITION: 
+#...    print("Something")
+#
+
+#count = 1
+
+#while count <= 4:
+    #print("looping")
+    #count += 1
+    
+#for TEMP_VARIABLE in SEQUENCE: 
+#...    pass
+
+colors = ['blue', 'green', 'red', 'purple']
+for color in colors:
+    print(color)
+    
+ages = {'kevin': 59, 'bob': 49, 'kayla': 21}
+for key in ages:
+    #print(key)
+    
+
+    
+    
+    
